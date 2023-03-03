fc9d357c4d7a5e9e5daaa91f046240249faecd02
laludonisetiawan <laludonisetiawan@gmail.com>
2023-03-03 09:50:18 +0800

    create categories menggunakan fungsi create dari sequelize

--- app/api/categories/controller.js
+++ app/api/categories/controller.js
@@ -5,10 +5,27 @@ module.exports = {
     try {
       const categories = await Category.findAll({
         where: { user: req.user.id },
-        attributes: ['id','name']
+        attributes: ["id", "name"],
       });
       res.status(200).json({
         message: "Success Get All Categories",
+        data: categories,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  createCategories: async (req, res, next) => {
+    try {
+      const { name } = req.body;
+      const categories = await Category.create({
+        name: name,
+        user: req.user.id,
+      });
+
+      res.status(201).json({
+        message: "Success create categories",
         data: categories,
       });
     } catch (err) {
--- app/api/categories/router.js
+++ app/api/categories/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { auth } = require("../../middlewares/Auth");
 const controller = require("./controller");
 
-router.get("/categories",auth,controller.getAllCategories);
+router.get("/categories", auth, controller.getAllCategories);
+router.post("/categories", auth, controller.createCategories);
 
 module.exports = router;
